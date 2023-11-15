03333c5f79301a1f2abbeee84862c55c5e1be2bb
Erik Maung <infiniteerik123@gmail.com>
2023-11-14 20:03:15 -0800

    Implement react-router-dom. See full commit msg:
    You can now navigate between the Home page and the Profile page. I set
    it up so that when you do npm start, the localhost you go to will
    display 'Home'. And then, add '/profile' to the end of the URL to go to
    the Profile page, where it should display 'Profile'.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>pass</div>
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
