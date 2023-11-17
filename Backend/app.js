a396880b17cac562c71d67f2fc82f9fc7c8c9c9d
Ethan Bresk <ethanb1490@gmail.com>
2023-11-16 23:48:52 -0800

    installed dotenv and added environment variables

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,19 @@
+// Require env variables
+require("dotenv").config(); 
+
 // Express setup
 const express = require('express');
 const app = express();
+
+// Mongoose setup
+const mongoose = require("mongoose");
+mongoose.connect(
+    process.env.MONGODB_URI, 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
  
 // Default request
 app.get('/', (req, res) => { 
@@ -9,8 +22,7 @@ app.get('/', (req, res) => {
 }) 
  
 // Port assignment
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT;
  
 // Server Setup
-app.listen(PORT,console.log(
-  `Server started on port ${PORT}`));
+app.listen(PORT,console.log(`Server started on port ${PORT}`));
