aa1a9a7136ab1cb1fee639ae66128c8561277ca9
aaronkwan <123356351+aaronkwan@users.noreply.github.com>
2023-11-20 21:20:40 -0800

    spotify integration

--- Frontend/src/pages/Login.js
+++ Frontend/src/pages/Login.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { loginWithSpotifyClick, logoutClick, refreshTokenClick } from '../utilities/spotify_integration.js';
 
 const Login = () => {
   return (
-    <div className='login'>Login</div>
+    <div className='login'>Login
+      <button onClick={() => { loginWithSpotifyClick() }}>LOGIN NOW</button>
+      <button onClick={() => { logoutClick() }}>LOGOUT NOW</button>
+      <button onClick={() => { refreshTokenClick() }}>REFRESH NOW</button>
+    
+    </div>
   )
 }
 
