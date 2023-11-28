9eac7407c406be256e9d2afac9e004dda3993fe2
Kirteous <grewalkirt0815@gmail.com>
2023-11-28 15:49:26 -0800

    connected search bar to database can currently search by author; TODO: will make the search results clickable that will lead to profile page of author; will also add feature to search by blog title instead that will have link leading to blog page

--- Frontend/src/components/searchbar.js
+++ Frontend/src/components/searchbar.js
@@ -1,30 +1,12 @@
 import { TextField, ListItem, ListItemText } from "@mui/material";
 import useFetch from "../hooks/useFetch"
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 export const SearchBar = () => {
-    const dummyNames = [
-        {
-            "id": 1,
-            "author": "Kirt"
-        },
-        {
-            "id": 2,
-            "author": "Erik"
-        },
-        {
-            "id": 3,
-            "author": "Ethan"
-        },
-        {
-            "id": 4,
-            "author": "Aaron"
-        },
-        {
-            "id": 5,
-            "author": "James"
-        },
-    ];
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8080/blogs', {
+        mode: 'no-cors',
+      });
 
     const[searchQuery, setSearchQuery] = useState("")
 
@@ -32,7 +14,7 @@ export const SearchBar = () => {
         setSearchQuery(text.target.value.toLowerCase())
     };
 
-    const searchedNames = dummyNames.filter((names) => {
+    const searchedNames = blogs.blogs.filter((names) => {
         return names.author.toLowerCase().includes(searchQuery);
     });
 
