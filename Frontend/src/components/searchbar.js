47d3b98941a4375a1ce247bf1ebcb2c3e82678db
Kirteous <grewalkirt0815@gmail.com>
2023-11-28 18:53:27 -0800

    small fix when because of error with undefined blogs

--- Frontend/src/components/searchbar.js
+++ Frontend/src/components/searchbar.js
@@ -1,12 +1,33 @@
 import { TextField, ListItem, ListItemText } from "@mui/material";
 import useFetch from "../hooks/useFetch"
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
 
 export const SearchBar = () => {
     const { data: blogs, isPending, error } = useFetch('http://localhost:8080/blogs', {
         mode: 'no-cors',
       });
+    const dummyNames = [
+        {
+            "id": 1,
+            "author": "Kirt"
+        },
+        {
+            "id": 2,
+            "author": "Erik"
+        },
+        {
+            "id": 3,
+            "author": "Ethan"
+        },
+        {
+            "id": 4,
+            "author": "Aaron"
+        },
+        {
+            "id": 5,
+            "author": "James"
+        },
+    ];
 
     const[searchQuery, setSearchQuery] = useState("")
 
@@ -14,8 +35,8 @@ export const SearchBar = () => {
         setSearchQuery(text.target.value.toLowerCase())
     };
 
-    const searchedNames = blogs.blogs.filter((names) => {
-        return names.author.toLowerCase().includes(searchQuery);
+    const searchedNames = blogs?.blogs?.filter((titles) => {
+        return titles.author.toLowerCase().includes(searchQuery);
     });
 
     return(
