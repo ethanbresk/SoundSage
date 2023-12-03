5e68eb3441751bf8c875e4ea31853cf60fe4dd81
Erik Maung <infiniteerik123@gmail.com>
2023-12-03 07:05:07 -0800

    branch into new Spotify API database feature

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -13,6 +13,7 @@ import { ThemeSwitch, useThemeContext, lightTheme, darkTheme } from './component
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {useState} from 'react';
 import { useTheme } from '@mui/system';
+import SongDatabase from './components/SongDatabase';
 
 function App() {
 
@@ -22,12 +23,13 @@ function App() {
       component = {<div className='App'>
         <TopBar />
         <Routes>
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/profile/:user" element={<OtherProfiles />} />
           <Route path="/create" element={<Create />} />
           <Route path="/blogs/:id" element={<BlogPage />} />
           <Route path="/settings" element = {<Settings />} />
+          <Route path="/SongDatabase" element={<SongDatabase />} />
           <Route path="*" element={<NotFound />}/>
         </Routes>
       </div>
