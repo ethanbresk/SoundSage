9eeef32ab450b96499ddc55b1af201394f9809cd
James Feeney <dear.jameschan@gmail.com>
2023-12-02 21:52:21 -0800

    fixed bug

--- Frontend/src/components/notificationtray.js
+++ Frontend/src/components/notificationtray.js
@@ -2,11 +2,9 @@ import React, { useState } from "react";
 import { BroswerRouter as Router, Routes, Route } from "react-router-dom";
 import { Box, ListItem, ListItemText } from "@mui/material";
 import { useTheme } from "@mui/system";
-import NotificationsIcon from "@mui/icons-material/Notifications";
 import Badge from "@mui/material/Badge";
 import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
-import MenuItem from "@material-ui/core/MenuItem";
 
 const Notification = ({ message }) => {
   return <div>{message}</div>;
@@ -22,13 +20,9 @@ const NotificationTray = ({ notifications }) => {
   return (
     <div>
       <IconButton>
-        <Badge color="secondary" variant="dot">
-          <NotificationsIcon />
-        </Badge>
+        <Badge color="secondary" variant="dot"></Badge>
       </IconButton>
-      <Menu>
-        <MenuItem></MenuItem>
-      </Menu>
+      <Menu></Menu>
     </div>
   );
 };
--- Frontend/src/components/searchbar.js
+++ Frontend/src/components/searchbar.js
@@ -1,65 +1,53 @@
 import { TextField, ListItem, ListItemText } from "@mui/material";
-import useFetch from "../hooks/useFetch"
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-<<<<<<< HEAD
-import { getPosts } from '../utilities/backend_integration.js';
+import useFetch from "../hooks/useFetch";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useTheme } from "@mui/system";
+import { getPosts } from "../utilities/backend_integration.js";
 
 export const SearchBar = () => {
-    const [isPending, setIsPending] = useState(true);
-    const [blogs, setBlogs] = useState(0);
+  const [isPending, setIsPending] = useState(true);
+  const [blogs, setBlogs] = useState(0);
 
-    useEffect(() => {
-        getPosts(null)
-        .then((res) => {
-        setBlogs(res);
-        setIsPending(false);
-        })
-    }, []);
-    console.log(blogs)
-=======
-import { useTheme } from '@mui/system';
-
-export const SearchBar = () => {
-    const theme = useTheme()
-
-    const { data: blogs, isPending, error } = useFetch('http://localhost:8080/getPosts', {
-        mode: 'no-cors',
-      });
->>>>>>> 0d708dc70c94bfd5d61f08ec2d267f5aa6ddf955
-
-    const[searchQuery, setSearchQuery] = useState("")
-
-    const handleChange = (text) => {
-        setSearchQuery(text.target.value.toLowerCase())
-    };
-
-    const searchedNames = blogs?.blogs?.filter((text) => {
-        return text.post_title.toLowerCase().includes(searchQuery);
+  useEffect(() => {
+    getPosts(null).then((res) => {
+      setBlogs(res);
+      setIsPending(false);
     });
+  }, []);
+  console.log(blogs);
 
-    return(
-        <div>
-        <TextField
-            variant="filled"
-            label="Search"
-            onChange={handleChange}
-            sx=
-            {{
-                top: 10,
-                width:600,
-                backgroundColor: "#EAF6FF"
-            }}
-        />
-        {searchQuery != "" && searchedNames.length > 0 && (
-            searchedNames.map((title, index) => (
-            <ListItem key={title.user_id}>
-                <Link to={`/blogs/${title.user_id}`}>
-                    <ListItemText primary={title.post_title}/>
-                </Link>
-            </ListItem>
-            ))
-        )}
-        </div>
-    )
-}
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleChange = (text) => {
+    setSearchQuery(text.target.value.toLowerCase());
+  };
+
+  const searchedNames = blogs?.blogs?.filter((text) => {
+    return text.post_title.toLowerCase().includes(searchQuery);
+  });
+
+  return (
+    <div>
+      <TextField
+        variant="filled"
+        label="Search"
+        onChange={handleChange}
+        sx={{
+          top: 10,
+          width: 600,
+          backgroundColor: "#EAF6FF",
+        }}
+      />
+      {searchQuery != "" &&
+        searchedNames.length > 0 &&
+        searchedNames.map((title, index) => (
+          <ListItem key={title.user_id}>
+            <Link to={`/blogs/${title.user_id}`}>
+              <ListItemText primary={title.post_title} />
+            </Link>
+          </ListItem>
+        ))}
+    </div>
+  );
+};
