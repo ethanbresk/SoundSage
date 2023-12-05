ba570d9dd3b691dec3a03ca05d6840fa1681c89f
Kirteous <grewalkirt0815@gmail.com>
2023-12-05 15:54:32 -0800

    fixed profile pages

--- Frontend/src/pages/Profile.js
+++ Frontend/src/pages/Profile.js
@@ -39,11 +39,11 @@ const Profile = () => {
     return (
       <div>
         <h1 className='page_header'>Hello, {username}!</h1>
-          <img src={picture_url} alt="Profile" width="100" height="100" position="static" class="center"
+        <div style={{color: theme.palette.text.main, textAlign: 'center'}}>
+        <img src={picture_url} alt="Profile" width="100" height="100"
             style={{borderRadius: 1000, left:300,alignItems: 'center'}}></img>
-        <div style={{color: theme.palette.text.main, textAlign: 'left'}}>
-          <h2>Total Likes: {total_likes}</h2>
-          <h2><a href={spotify_url}>Your Spotify Page</a></h2>
+          <h2><a href={spotify_url}>Your Spotify Page</a>
+          </h2>
         </div>
       </div>
     );
@@ -59,10 +59,10 @@ const Profile = () => {
         zIndex={2}
       />
       <div className="center">
-      {data ? userDisplay(data) : <h1 style={{textAlign: 'center'}}>Login to Display Your Info!</h1>}
+      {data ? userDisplay(data) : <h1 style={{textAlign: 'center' }}>Login to Display Your Info!</h1>}
       <div className="blog_home" style = {{ paddingBottom:100,height:415,overflowY: 'auto' }}>
         {isPending && <div>Loading...</div>}
-        {blogs && <BlogCollection blogs={blogs.blogs} title="Kirt Blogs:" />}
+        {blogs && <BlogCollection blogs={blogs.blogs} title="Kirt Blogs:"/>}
         </div>
     </div>
     </div>
