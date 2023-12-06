0c9333e9c8f69357252b2cb7f1e4c8b05cfcb267
Kirteous <grewalkirt0815@gmail.com>
2023-12-06 09:56:10 -0800

    theme switching for text adding for blog collection

--- Frontend/src/components/BlogCollection.js
+++ Frontend/src/components/BlogCollection.js
@@ -25,13 +25,13 @@ const BlogCollection = ({ blogs, title }) => {
                   <Link to={`/blogs/${blog._id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
                     <Avatar src={blog.user_picture} style={{ width: '80px', height: '80px', marginBottom: '5px' }}/>
                     <div style={{ marginLeft: '15px', textAlign: 'left', fontFamily: 'monospace' }}> {/*experiment with textalign center or left*/}
-                      <Typography component="div" variant="h6" color="textPrimary" fontFamily={'monospace'}>
+                      <Typography component="div" variant="h6" fontFamily={'monospace'} style={{ color: theme.palette.text.main}}>
                         {blog.post_title}
                       </Typography>
-                      <Typography component="div" variant="body2" color="textSecondary" fontFamily={'monospace'}>
+                      <Typography component="div" fontFamily={'monospace'} style={{ color: theme.palette.text.main}}>
                         By {blog.user_name}
                       </Typography>
-                      <Typography component="div" variant="body1" color="textPrimary" fontFamily={'monospace'}>
+                      <Typography component="div" variant="body1" fontFamily={'monospace'} style={{ color: theme.palette.text.main}}>
                         {blog.post_body}
                       </Typography>
                       <Link to={blog.song_url} style={{ width: '100%', textDecoration: 'none', textAlign: 'center' }}>
@@ -45,8 +45,8 @@ const BlogCollection = ({ blogs, title }) => {
                         </div>
                       </Link>
                       <div style={{ marginTop: '20px', display: 'flex', alignItems: 'center' }}>
-                        <IconButton size="small" disabled><ThumbUpAltIcon fontSize="small" /></IconButton>
-                        <Typography variant="body2" color="textSecondary">
+                        <IconButton size="small" disabled><ThumbUpAltIcon fontSize="small" style={{ color: theme.palette.accentOne.main}} /></IconButton>
+                        <Typography variant="body2" style={{ color: theme.palette.text.main}}>
                           {blog.num_of_likes}
                         </Typography>
                       </div>
