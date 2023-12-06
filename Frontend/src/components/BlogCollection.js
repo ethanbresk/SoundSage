c071b58b842d31e7a56d58ade4fda54c955225c0
James Feeney <dear.jameschan@gmail.com>
2023-12-05 17:23:35 -0800

    work in progress blogcollection

--- Frontend/src/components/BlogCollection.js
+++ Frontend/src/components/BlogCollection.js
@@ -1,10 +1,13 @@
 import React, { useEffect } from 'react'
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import { Box } from '@mui/material'
+import { Box, Avatar} from '@mui/material'
 import { useTheme } from '@mui/system';
 import { getUser } from '../utilities/backend_integration.js';
 import { Card } from 'react-bootstrap';
+import Button from '@mui/material/Button';
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import Typography from '@mui/material/Typography';
 
 
 
@@ -25,21 +28,35 @@ const BlogCollection = ({ blogs, title }) => {
             alignItems= "center"
           >
             {blog.parent ? null :
-              (<Link to={`/blogs/${blog._id}`}>
-                <img src={blog.user_picture} width="100" height="100"></img>
-                <p style={{textAlign: 'left', paddingLeft: 15, paddingTop: 5}}><em>{blog.user_name}</em></p>
-                <h2 style={{color: theme.palette.text.main}}>{blog.post_title}</h2>
-                <p>{blog.post_body}</p>
-                  <Link to={blog.song_url}>
-                    <Card>
-                      <Card.Img src={blog.song_pic}/>
-                      <Card.Body>
-                        <Card.Title>{blog.song_name}</Card.Title>
-                      </Card.Body>
-                    </Card>
-                  </Link>
-                <p style={{textAlign: 'right', paddingRight: 15, paddingBottom: 5}}>Likes: <b>{blog.num_of_likes}</b></p>
-              </Link>)
+              (
+                <Link to={`/blogs/${blog._id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                  <Avatar src={blog.user_picture} style={{ margin: 'auto', marginTop: '15px' }} />
+
+                  <div style={{ marginLeft: '15px' }}>
+                    <Typography component="div" variant="body2" color="textSecondary">
+                      <em>{blog.user_name}</em>
+                    </Typography>
+                    <Typography component="div" variant="h6" color="textPrimary">
+                      {blog.post_title}
+                    </Typography>
+                    <Typography component="div" variant="body1" color="textPrimary">
+                      {blog.post_body}
+                    </Typography>
+                    <Link to={blog.song_url} style={{ width: '100%' }}>
+                      <Card>
+                        <Card.Img src={blog.song_pic} />
+                        <Card.Body>
+                          <Card.Title>{blog.song_name}</Card.Title>
+                        </Card.Body>
+                      </Card>
+                    </Link>
+                    <div style={{ marginTop: '20px' }}>
+                      <ThumbUpAltIcon fontSize="small" />
+                      <span style={{ marginLeft: '5px' }}>{blog.num_of_likes}</span>
+                    </div>
+                  </div>
+                </Link>
+              )
             }
           </Box>
           </div>
